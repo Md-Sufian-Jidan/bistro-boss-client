7b282cf3a2cba5410c49ab9cdd368859f6f9a1b8
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-16 12:00:33 +0600

    featured section css added

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -1,8 +1,22 @@
-
+import SectionTitle from "../../../Components/sectionTitle/SectionTitle";
+import featuredImg from '../../../assets/home/featured.jpg'
+import './Featured.css'
 const Featured = () => {
     return (
-        <div>
-            
+        <div className="featured-item text-white pt-8 my-20">
+            <SectionTitle subHeading="Check it Out" heading="Featured Item"></SectionTitle>
+            <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+                <div>
+                    <img src={featuredImg} alt="" />
+                </div>
+                <div className="md:ml-10 space-y-3">
+                    <p>Aug 20, 2024</p>
+                    <p className="uppercase">Where Can i get some?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit recusandae excepturi earum facilis ullam qui dicta quisquam voluptates cum a dolores, tempore quos, nihil similique reprehenderit est veritatis alias rerum. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat porro, officiis eum natus explicabo maxime esse? Delectus, nemo. Nobis tempore odio a atque, voluptate ratione laborum porro assumenda enim id!
+                    </p>
+                    <button className="btn btn-outline">Order Now</button>
+                </div>
+            </div>
         </div>
     );
 };
