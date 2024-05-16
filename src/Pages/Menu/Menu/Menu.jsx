d043b593dd0c116574adedc3a4b0eab17cc91e5f
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-16 21:59:53 +0600

    menu category added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,15 @@
 import { Helmet } from "react-helmet-async";
-
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h2>men men menu</h2>
+            <Cover img={menuImg} title="Our Menu" />
+            <PopularMenu />
         </div>
     );
 };
