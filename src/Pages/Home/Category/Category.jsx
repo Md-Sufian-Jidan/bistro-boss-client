fc5806804abcd2ed4598bb0a55f75d21b3ca5a92
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-16 10:32:45 +0600

    section title component added

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -8,10 +8,15 @@ import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from '../../../Components/sectionTitle/SectionTitle';
 
 const Category = () => {
     return (
-        <Swiper
+        <section>
+            <SectionTitle heading={"Order Online"}
+            subHeading={"From 11.00 am to 10.00 pm"}>
+            </SectionTitle>
+            <Swiper
             slidesPerView={4}
             spaceBetween={30}
             centeredSlides={true}
@@ -42,6 +47,7 @@ const Category = () => {
                 <h3 className="text-4xl uppercase text-center -mt-16 text-white">Salads</h3>
             </SwiperSlide>
         </Swiper>
+        </section>
     );
 };
 
