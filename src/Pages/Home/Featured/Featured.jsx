404eb76acc5f4704f3ae1c3edceee9312e6dbf28
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-16 12:20:02 +0600

    testimonial section added

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -3,9 +3,9 @@ import featuredImg from '../../../assets/home/featured.jpg'
 import './Featured.css'
 const Featured = () => {
     return (
-        <div className="featured-item text-white pt-8 my-20">
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle subHeading="Check it Out" heading="Featured Item"></SectionTitle>
-            <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+            <div className="md:flex justify-center items-center pb-20 pt-12 px-36 bg-opacity-40 bg-gray-400">
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -14,7 +14,7 @@ const Featured = () => {
                     <p className="uppercase">Where Can i get some?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit recusandae excepturi earum facilis ullam qui dicta quisquam voluptates cum a dolores, tempore quos, nihil similique reprehenderit est veritatis alias rerum. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat porro, officiis eum natus explicabo maxime esse? Delectus, nemo. Nobis tempore odio a atque, voluptate ratione laborum porro assumenda enim id!
                     </p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
         </div>
