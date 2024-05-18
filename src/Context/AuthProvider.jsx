6191f3b4235ce763fdd7ec3b3363409897758670
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-18 22:29:35 +0600

    context basic set up complete

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,11 +1,18 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState();
-    const userInfo = { user };
+    const [loading, setLoading] = useState(true);
+    // create user 
+    const createUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(email, password);
+    };
+    const authInfo = { user };
     return (
-        <AuthContext.Provider value={userInfo}>
+        <AuthContext.Provider value={authInfo}>
             {children}
         </AuthContext.Provider>
     );
